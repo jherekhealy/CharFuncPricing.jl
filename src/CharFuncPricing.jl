0cdf47137e4f8739fb8c80fce198e1a00c5632eb
Jherek Healy <jherekhealy@protonmail.com>
2020-06-05 15:05:49 +0200

    reduce sin, cos evaluations
    Change-Id: I394fb0436dae1cf7b7de92c7fbc18c35d3e6bd71

--- src/CharFuncPricing.jl
+++ src/CharFuncPricing.jl
@@ -3,12 +3,12 @@ include("Heston.jl")
 
 export CosCharFuncPricer, makeCosCharFuncPricer, priceEuropean
 
-struct CosCharFuncPricer{T,CT}
+struct CosCharFuncPricer{T}
     τ::T
     a::T
     b::T
     uk::Vector{T}
-    phi::Vector{CT}
+    phi::Vector{T}
     pi::T
 end
 
@@ -18,18 +18,20 @@ function makeCosCharFuncPricer(CC, R, pi::T, p::HestonParams{T}, τ::T, m::Int, 
     c2 = c2 + sqrt(abs(c4))
     a = c1 - l * sqrt(abs(c2))
     b = c1 + l * sqrt(abs(c2))
-
-    phi = zeros(CC, m)
+    phi = zeros(R, m)
     for i = 1:m
         z = i * pi / (b - a)
-        phi[i] = evaluateCharFunc(CC, p, z, τ)
+        phiz = evaluateCharFunc(CC, p, z, τ)
+        sz = sin(-z * a)
+        cz = cos(-z * a)
+        phi[i] = real(phiz) * cz - imag(phiz) * sz
     end
     uk = zeros(R, m)
     return CosCharFuncPricer(τ, a, b, uk, phi, pi)
 end
 
 #we adopt here the alternative formula of LeFloch "More Robust Pricing of European Options Based on Fourier Cosine Series Expansions"
-function priceEuropean(p::CosCharFuncPricer{T,CT}, isCall::Bool, strike::T, forward::T, discountDf::T) where {T,CT}
+function priceEuropean(p::CosCharFuncPricer{T}, isCall::Bool, strike::T, forward::T, discountDf::T) where {T,CT}
     pricePut = 0
     x = log(forward / strike)
     if x >= -p.a && x >= p.b
@@ -58,15 +60,10 @@ function priceEuropean(p::CosCharFuncPricer{T,CT}, isCall::Bool, strike::T, forw
             coeff = 2 / (b - a) * (-chi + estrike * psi)
             uk[i] = coeff
         end
-        x = 0 #log(forward / f)
         phi0 = 1 #real(cfi.phi[0])
         sumPut = (phi0) * uk0 / 2
         for k = 1:length(uk)
-            z = k * pi / (p.b - p.a)
-            sz = sin(z * (x - p.a))
-            cz = cos(z * (x - p.a))
-            rephi = real(p.phi[k]) * cz - imag(p.phi[k]) * sz
-            sumPut += rephi * p.uk[k]
+            sumPut += p.phi[k] * p.uk[k]
         end
         pricePut = discountDf * f * sumPut
     end
