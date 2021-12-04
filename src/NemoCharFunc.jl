f2efa1ca45620cc85a8818379abfea217a4e2ec8
Jherek Healy <jherekhealy@protonmail.com>
2021-12-04 14:16:01 +0100

    remove nemo expm definition
    Change-Id: I181ff73fe58bbe8c164f9c3fd4948f2ae6c7f5b2

--- src/NemoCharFunc.jl
+++ src/NemoCharFunc.jl
@@ -11,5 +11,5 @@ end
 @inline CharFuncPricing.model(cf::NemoCharFunc) = cf.model
 @inline CharFuncPricing.oneim(cf::NemoCharFunc) = onei(cf.field)
 @inline CharFuncPricing.const_pi(cf::NemoCharFunc) = real(Nemo.const_pi(cf.field))
-@inline Base.expm1(x::acb) = exp(x) - 1 #for Nemo - now in latest Nemo 0.19.2
+#@inline Base.expm1(x::acb) = exp(x) - 1 #for Nemo - now in latest Nemo 0.19.2
 @inline Base.sign(x::arb) = x < 0 ? -1 : 1
